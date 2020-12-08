6406cbc8ed0a7204a72cfbb42e1e1e73de73b67f
Dmytro Dovbii <dmytro.d@qualisystems.com>
2020-12-08 15:02:48 +0000

    Show application links

--- colony-deck/src/ColonyStartSandboxStageExecutionDetails.tsx
+++ colony-deck/src/ColonyStartSandboxStageExecutionDetails.tsx
@@ -7,11 +7,26 @@ import {
 } from '@spinnaker/core'
 
 export function ColonyStartSandboxStageExecutionDetails(props: IExecutionDetailsSectionProps) {
+  var apps = props.stage.outputs.QuickLinks;
+  const items=[];
+  if(apps){
+    for(let app of apps) {
+        const href=[]
+        for(let link of app.shortcuts){
+        href.push(<li><a href="{link}">{link}</a></li>)
+        }
+        items.push(<p><b>{app.name}: </b>{href}</p>)
+    }
+  }
+
   return (
     <ExecutionDetailsSection name={props.name} current={props.current}>
       <StageFailureMessage stage={props.stage} message={props.stage.outputs.failureMessage} />
       <div>
         <p>Started sandbox with id: <b> {props.stage.outputs.sandboxId} </b></p>
+        <div>
+            {items}
+        </div>
       </div>
     </ExecutionDetailsSection>
   );
