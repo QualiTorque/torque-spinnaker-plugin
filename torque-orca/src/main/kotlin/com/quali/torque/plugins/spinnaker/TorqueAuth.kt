cd24b45822eceb70ebfeb2f470edfe68a3d1ca3d
Dmytro Dovbii <dmytro.d@quali.com>
2022-08-11 09:18:13 +0300

    add datetime formatter to client

--- torque-orca/src/main/kotlin/com/quali/torque/plugins/spinnaker/TorqueAuth.kt
+++ torque-orca/src/main/kotlin/com/quali/torque/plugins/spinnaker/TorqueAuth.kt
@@ -3,10 +3,8 @@ package com.quali.torque.plugins.spinnaker
 import com.quali.torque.client.ApiClient
 import com.quali.torque.client.Configuration
 import com.quali.torque.plugins.spinnaker.utils.StatusService
-import java.time.format.DateTimeFormatter
 import java.time.format.DateTimeFormatterBuilder
 import java.time.temporal.ChronoField
-
 
 class TorqueAuth(private val token: String, private val url: String) {
     fun getClient(): ApiClient {
@@ -14,16 +12,15 @@ class TorqueAuth(private val token: String, private val url: String) {
         val defaultClient: ApiClient = Configuration.getDefaultApiClient()
         defaultClient.basePath = url
         defaultClient.setUserAgent("Torque-Plugin-Bamboo/$version")
-        defaultClient.setApiKey(String.format("Bearer %s", token))
-        defaultClient.connectTimeout = 60 * 1000
-        defaultClient.readTimeout = 60 * 1000
-        val f: DateTimeFormatter = DateTimeFormatterBuilder()
-            .appendPattern("[yyyy-MM-dd'T'HH:mm:ss.SSSSSSSXXX][yyyy-MM-dd'T'HH:mm:ss.SSSSSS][yyyy-MM-dd'T'HH:mm:ss]")
+        val f = DateTimeFormatterBuilder()
+            .appendPattern("[yyyy-MM-dd'T'HH:mm:ss.SSSSSSSXXX][yyyy-MM-dd'T'HH:mm:ss.SSSSSXXX][yyyy-MM-dd'T'HH:mm:ss.SSSSSS][yyyy-MM-dd'T'HH:mm:ss.SSSSS][yyyy-MM-dd'T'HH:mm:ss]")
             .parseLenient()
             .parseDefaulting(ChronoField.OFFSET_SECONDS, 0)
             .toFormatter()
         defaultClient.setOffsetDateTimeFormat(f)
+        defaultClient.setApiKey(String.format("Bearer %s", token))
+        defaultClient.connectTimeout = 60 * 1000
+        defaultClient.readTimeout = 60 * 1000
         return defaultClient
-
     }
 }
